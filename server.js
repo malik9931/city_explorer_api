6d3043d4f8deb552c4f3cfc3dd5ce5bb93666e76
Malek <engmalek.civil@gmail.com>
2021-02-21 14:26:22 +0200

    adding some stuff

--- server.js
+++ server.js
@@ -14,7 +14,15 @@ server.use(cors()); // make it opened
 const PORT = process.env.PORT || 3030;
 
 
-
+// handle any route
 server.listen(PORT, ()=>{
     console.log(`Listening on PORT ${PORT}`);
+})
+
+
+// location route
+//request: localhost:3000/location
+server.get('/location',(req,res)=>{
+    const locData = require('./data/location.json');
+    console.log(locData);
 })
